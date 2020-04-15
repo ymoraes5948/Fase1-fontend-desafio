221a96fcec6a0bb1e7443f96768ab9ae2e451cda
ymoraes5948 <50243915+ymoraes5948@users.noreply.github.com>
2020-04-14 21:18:12 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api
+      .post("repositories", {
+        title: "Umbriel",
+        url: "https://github.com/rocketseat/umbriel",
+        techs: ["Node.js", "ReactJs"],
+      })
+      .then((response) => setRepositories([...repositories, response.data]));
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter((repository) => repository.id !== id));
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
